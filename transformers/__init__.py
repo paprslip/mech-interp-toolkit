2a46fe77537e4a081f0190e9dbe0e2e7bc73c862
terrence <terrencezhang18@gmail.com>
2025-11-24 20:30:25 -0500

    added init file for transformers package. note prev commit where transformers package is added main.py is not working, codebase is WIP

---
+++ transformers/__init__.py
@@ -0,0 +1,5 @@
+from .logit_lens import logit_lens, logit_evolution, project_to_logits
+from .patching import patching, patch_layer, patch_layer_kv
+
+__author__ = "Terrence Zhang"
+__email__ = "terrencez.zhang@mail.utoronto.ca"
